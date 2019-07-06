69ac3726689b260498419d24ef1ac4f2b10a019a
jlpeterson695 <jlpeterson695@gmail.com>
2019-07-06 14:19:55 -0700

    whatever

--- day.js
+++ day.js
@@ -1,5 +1,6 @@
 // Returns the day of the week for the given date.
-function dayName(var) {
-  const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  return daysOfTheWeek[var.getDay()];
+function dayName(date) {
+  const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday",
+                         "Thursday", "Friday", "Saturday"];
+  return daysOfTheWeek[date.getDay()];
 }
